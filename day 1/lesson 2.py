b61b2302c7e7b91fd3dfca245426f442591bb7ca
gikako1 <nikavanishvilinika@gmail.com>
2024-05-17 17:25:29 +0400

    day1 lesson 2

---
+++ day 1/lesson 2.py
@@ -0,0 +1,6 @@
+name="nika"
+surname="vanishvili"
+height="178"
+age="15"
+
+print("hello,my name is"+" "+ str(name) + " " + str(surname) +", i am" + " " + str(age)+ " " + "years old" + ", " + "i am"+ " " + str(height) +" "+ "meters tall." ) 
