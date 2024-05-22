449e0663108757ba37e08a5c87cbd630bc9b8345
gikako1 <nikavanishvilinika@gmail.com>
2024-05-22 23:24:38 +0400

    homework(final boss)
    ცინცხალი დავალება

--- level3/home work/level3.py
+++ level3/home work/level3.py
@@ -12,3 +12,5 @@ blue_ball=11
 
 print(red_ball+blue_ball)
 
+#print(red_ball+blue_ball) ბრძანება იმიტომ მუშაობს რადგან red_ball-ს მივანიჭეთ მთელი რიცხვი 5-ის მნიშვნელობა.
+#იგივე გავაკეთეთ blue_ball-თან, რადგანაც red_ball და blue_ball მთელი რიცხვები არიან, ისინი პრობლემის გარეშე შეიკრიბნენ.
