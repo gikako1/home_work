b61766a801904900c38b30db362ed5fc42a96b7d
gikako1 <nikavanishvilinika@gmail.com>
2024-06-01 16:51:57 +0400

    HOMEWORK

---
+++ level5/home work/level5.py
@@ -0,0 +1,23 @@
+name="ნიკა "
+surname="ვანიშვილი "
+age="15 წლის "
+address="საქართველო "
+print(name + surname+age+address)
+#როდესაც მე ერთმანეთს მივუმატე ცვლადები, კომპიუტერმა ეკრანზე აჩვენა მათი მნიშვნელობები
+#რადგანაც მათი მნიშვნელობები string ები არის, კომპიუტერმა ჩვეულებრივ ამობეჭდა ისინი ეკრანზე.
+
+num1=1
+num2=2
+num3=3
+num4=4
+num5=5
+num6=6
+num7=7
+num8=8
+num9=9
+num10=10
+
+print(num1+num2)
+print(num3-num4)
+print(num5*num6)
+print(num7/num8)
