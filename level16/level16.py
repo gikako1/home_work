f2bc3ddc1c664b7a42330301f2d27e09c42bb94e
gikako1 <nikavanishvilinika@gmail.com>
2024-07-10 00:00:41 +0400

    homework

---
+++ level16/level16.py
@@ -0,0 +1,48 @@
+# Pop
+#დავალება 1
+number=[1,2,3,4]
+number.pop(3)
+print(number)
+
+#დავალება 2
+number.pop(0)
+number.append(1)
+print(number)
+
+#დავალება 3
+number.pop(2)
+print(number)
+
+#დავალება 4
+number.pop(1)
+print(number)
+
+# Count
+#დავალება 1
+num=[1,5,6,5,7,5]
+x=num.count(5)
+print(x)
+
+#დავალება 2 ვერ გავაკეთე
+
+#დავალება 3
+boolean=[True,True,False,True,False]
+z=boolean.count(True)
+print(z)
+
+#დავალება 4
+list=[[3,4],[4,5],[0,7]]
+sub=list.count([3,4])
+print(sub)
+
+#Len
+#დავალება 1
+numbers=[1,2,3,4,5,6,7,8,9,10]
+print(len(numbers))
+#დავალება 2
+weekdays=["mon","tue","wed","thu","fri","sat","sun"]
+print(len(weekdays))
+
+#დავალება 3
+nestlist=[[1,2],[3,4],[5,6]]
+print(len(nestlist))
