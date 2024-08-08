d391fd13fd1a7bb081c193f66ffd2307ff0dd943
gikako1 <nikavanishvilinika@gmail.com>
2024-08-08 12:23:11 +0400

    dzalian vecade gaketeba,dzaalian rtuli davalebebia aq.

--- level20/level20.py
+++ level20/level20.py
@@ -1,4 +1,26 @@
-#დავალება 1 
+# 1. Write a function that takes a list of numbers and returns the sum of all the even numbers
 
+# 2. Create a function that accepts a string and returns the string reversed without using any built-in reverse functions.
+def reversename():
+    name=str(input())
+    print(name[::-1])
+    return
+reversename()
+# 3. Implement a function that takes a number and returns the factorial of that number using recursion.
 
-#დავალება 2
+# 4. Design a function that accepts two lists and returns a list of common elements without using set operations.
+
+# 5. Write a function that takes a string and returns the number of vowels in the string.
+word=str(input())
+syllables=["a","e","i","u","o","y"]
+def vowel():
+    
+    
+
+# 6. Create a function that accepts a list of integers and returns the list sorted in ascending order without using built-in sort functions.
+
+# 7. Implement a function that takes two strings and returns True if one string is a permutation of the other.
+
+# 8. Create a function that takes a number and returns True if it is a prime number.
+
+# 9. Design a function that accepts a list of strings and returns a list with the strings sorted by their lengths.
