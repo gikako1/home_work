c59cf219069914ad011f1faf54456df3df09d759
gikako1 <nikavanishvilinika@gmail.com>
2025-01-29 22:48:46 +0400

    Update homework.js

---
+++ level74/homework.js
@@ -0,0 +1,21 @@
+function carconstructor(brand, model, year, engine) {
+  this.brand = brand;
+  this.model = model;
+  this.year = year;
+  this.engine = engine + "L";
+}
+let mercedes = new carconstruct("mercedes", "baymach", "2022", "3.0");
+
+function animalconstructor(name, type, food) {
+  this.name = name;
+  this.type = type;
+  this.food = food;
+}
+let mammoth = new animalconstruct("mammoth", "mammal", "vegan?");
+
+function studentconstructor(name, age, grades) {
+  this.name = name;
+  this.age = age;
+  this.grades = grades;
+}
+let stickler = new studentconstruct("stickler", "17", "A++");
