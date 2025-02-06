7de0e452a338fbb8cb0c610953957ac7fdcd0d52
gikako1 <nikavanishvilinika@gmail.com>
2025-02-07 01:44:48 +0400

    homework

--- level51/homework.py
+++ level51/homework.py
@@ -5,15 +5,16 @@
 #  ჯამები. წარმატებები.❤️
 def row_column_sums(matrix):
     row_sums = []
-    column_sums = [0] * len(matrix[0])  # სვეტების ჯამების დასაწყისი 0-ით
+    column_sums = [0] * len(matrix[0])
     
-    for i in range(len(matrix)):  # ვხვდებით თითოეული რიგის ჯამს
+    for i in range(len(matrix)):
         row_sum = 0
-        for j in range(len(matrix[i])):  # თითოეულ ელემენტს ვატყობ
+        for j in range(len(matrix[i])):
             row_sum += matrix[i][j]
-            column_sums[j] += matrix[i][j]  # ამავე დროს ვამატებთ სვეტს
+            column_sums[j] += matrix[i][j]
         row_sums.append(row_sum)
     
     return print("Row sums:", row_sums),print("Column sums:", column_sums)
+
 row_sums, column_sums = row_column_sums([[1,2],[2,3]])
 
